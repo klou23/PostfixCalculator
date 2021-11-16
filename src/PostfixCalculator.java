1e27185e8dee07ccf52b1c0b567d69b5555783f2
Kevin Lou <58053969+klou23@users.noreply.github.com>
2021-11-16 14:18:45 -0700

    Added evaluation of pFix strings

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,7 +1,26 @@
+import java.util.StringTokenizer;
+
 public class PostfixCalculator {
 
     public static void main(String[] args) {
 
+    }
+
+    public double pFix(String str){
+        StringTokenizer st = new StringTokenizer(str);
+        Stack<Double> s = new Stack<>();
+
+        while(st.hasMoreTokens()){
+            String token = st.nextToken();
+            if(token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")){
+                double a = s.pop();
+                double b = s.pop();
+                s.push(evaluate(a,b,token));
+            }else{
+                s.push(Double.parseDouble(token));
+            }
+        }
+        return s.peek();
     }
 
 }
