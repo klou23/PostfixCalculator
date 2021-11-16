6b90cd8b39b0c280eb69bc8a7fab72f868ba4e63
Trent Blair <tblair22@kentdenver.org>
2021-11-16 14:19:42 -0700

    I coded the evaluate method

--- src/PostfixCalculator.java
+++ src/PostfixCalculator.java
@@ -1,5 +1,14 @@
 public class PostfixCalculator {
 
+    public double evaluate(double a, double b, String op){
+        switch(op){
+            case "+": return a+b;
+            case "-": return a-b;
+            case "*": return a*b;
+            default: return a/b;
+        }
+    }
+
     public static void main(String[] args) {
 
     }
